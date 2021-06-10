3df55f1b699853711685e64e6fa3fd8ddd25d3a2
mohamed-zayton <47240722+mohamed-zayton@users.noreply.github.com>
2021-06-10 07:27:40 +0200

    Add iir filter (#4)
    * file
    
    * irr_review
    
    * irr_review
    
    * irr_review
    
    * irr_review
    
    * irr_review
    
    * irr_review
    
    * irr_filter
    
    * Dummy
    
    * Update helper_method.py
    
    * Update helper_method.py
    
    Co-authored-by: mohamed-zayton <mohamed-zayton>
    Co-authored-by: Youssef1313 <youssefvictor00@gmail.com>

--- helper_method.py
+++ helper_method.py
@@ -1,7 +1,6 @@
 from scipy import signal
 from .pzmap import pzmap
 
-
 freqs = [[0, 170],
          [170, 310],
          [310, 600],
@@ -11,6 +10,21 @@ freqs = [[0, 170],
          [6000, 12000],
          [12000, 14000],
          [14000, 16000]]
+
+
+def iir_filter(order, fs):
+    iir_filters = []
+    for i in range(len(freqs)):
+        lis = [freqs[i][0] / fs, freqs[i][1] / fs]
+        if lis[1] >= 1:
+            return iir_filters
+        if lis[0] == 0:
+            current_filter = signal.iirfilter(order, lis[1], btype='lowpass')
+        else:
+            current_filter = signal.iirfilter(order, lis)
+        iir_filters.append([current_filter])
+
+    return iir_filters
 
 
 def plot_zeros_poles(p_z):
