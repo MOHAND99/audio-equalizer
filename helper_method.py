e852b83b7d84289551331d8af401b7c5bd4b509c
Muhannad Muhammad Bashar <53486163+MOHAND99@users.noreply.github.com>
2021-06-10 07:23:34 +0200

    add pzmap method (#6)
    * add pzmap
    
    * z_p_plotting
    
    * z_p_plotting
    
    * Update helper_method.py
    
    * z_p_plotting
    
    * z_p_plotting
    
    * z_p_plotting
    
    * z_p_plotting
    
    Co-authored-by: mohamed-zayton <mohamed-zayton>
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

--- helper_method.py
+++ helper_method.py
@@ -1,3 +1,7 @@
+from scipy import signal
+from .pzmap import pzmap
+
+
 freqs = [[0, 170],
          [170, 310],
          [310, 600],
@@ -7,3 +11,9 @@ freqs = [[0, 170],
          [6000, 12000],
          [12000, 14000],
          [14000, 16000]]
+
+
+def plot_zeros_poles(p_z):
+    for ele in (p_z):
+        z = signal.TransferFunction(ele[0], ele[1])
+        pzmap(z.zeros, z.poles)
