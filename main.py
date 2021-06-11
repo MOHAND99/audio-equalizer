f7b59925305c3f66086b721be6fcf72cc71a0a1b
Muhannad Muhammad Bashar <53486163+MOHAND99@users.noreply.github.com>
2021-06-11 17:27:32 +0200

    helper method importing (#15)
    * helper method importing
    
    * Update main.py
    
    * Fix
    
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import tkinter as tk
+from tkinter import filedialog
 import soundfile as sf
 import numpy as np
-from helper_method import (get_bands, plot_mag_phase,
-                           iir_filters, fir_filters,
-                           plot_impl_unitstep, plot_zeros_poles)
+from helper_method import get_bands, plot_mag_phase,\
+                          iir_filters, fir_filters
 from scipy import signal
 
 root = tk.Tk()
 root.withdraw()
 filetypes = [("wav files", ".wav")]
-file_path = tk.filedialog.askopenfilename(title="Select .wav file",
-                                          filetypes=filetypes)
+file_path = filedialog.askopenfilename(title="Select .wav file",
+                                       filetypes=filetypes)
 data, fs = sf.read(file_path)
 data = np.asarray(data)
 print("File information:")
 print(f"Path: {file_path}")
 print(f"Data dimensions: {np.shape(data)}")
 print(f"Frequency: {fs}")
-
 bands = get_bands()
 gains = []
 for band in bands:
@@ -51,8 +50,8 @@ for i, filter in enumerate(filters_with_gain):
     output = output + current
 # TODO: Draw output in time and frequency domain
 
-output_file_name = tk.filedialog.asksaveasfilename(title="Save wav file",
-                                                   defaultextension='.wav',
-                                                   filetypes=filetypes)
+output_file_name = filedialog.asksaveasfilename(title="Save wav file",
+                                                defaultextension='.wav',
+                                                filetypes=filetypes)
 sf.write(output_file_name, output, output_fs)
 input("Press any key to exit...")
