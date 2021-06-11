7b0f57349232e6733ca9dc6266fc901a072ea952
mohamed-zayton <47240722+mohamed-zayton@users.noreply.github.com>
2021-06-11 08:50:22 +0200

    Plotting and general enhancements (#10)
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * imp_unit_mag_phase
    
    * Apply suggestions from code review
    
    * imp_unit_mag_phase
    
    * Update helper_method.py
    
    * Update helper_method.py
    
    * Update helper_method.py
    
    Co-authored-by: mohamed-zayton <mohamed-zayton>
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

--- helper_method.py
+++ helper_method.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pzmap import pzmap
-import scipy.signal
+from scipy import signal
+import matplotlib.pyplot as plt
 
 
 def get_bands():
@@ -21,12 +22,12 @@ def design_fir_system(fs, order=100, freqs=get_bands()):
 
     freqs[0] = freqs[0] / fs
     if freqs[0][1] < 1:
-        filters[0] = scipy.signal.firwin(order + 1, freqs[0][1])
+        filters[0] = signal.firwin(order + 1, freqs[0][1])
 
     for i in range(1, len(freqs)):
         freqs[i] = freqs[i] / fs
         if freqs[i][1] < 1:
-            filters.append(scipy.signal.firwin(order + 1, freqs[i]))
+            filters.append(signal.firwin(order + 1, freqs[i]))
 
     return filters
 
@@ -35,20 +36,66 @@ def iir_filter(order, fs):
     iir_filters = []
     bands = get_bands()
     for i in range(len(bands)):
-        lis = [bands[i][0] / fs, bands[i][1] / fs]
+        lis = [bands[i][0] * 2 / fs, bands[i][1] * 2 / fs]
         if lis[1] >= 1:
             return iir_filters
         if lis[0] == 0:
-            current_filter = scipy.signal.iirfilter(order,
-                                                    lis[1], btype='lowpass')
+            current_filter = signal.iirfilter(
+                                            order, lis[1],
+                                            output='zpk',
+                                            btype='lowpass')
         else:
-            current_filter = scipy.signal.iirfilter(order, lis)
+            current_filter = signal.iirfilter(order, lis, output='zpk')
         iir_filters.append([current_filter])
 
     return iir_filters
 
 
-def plot_zeros_poles(p_z):
-    for ele in (p_z):
-        z = scipy.signal.TransferFunction(ele[0], ele[1])
-        pzmap(z.zeros, z.poles)
+def plot_zeros_poles(poles_zeros):
+    for ele in poles_zeros:
+        pzmap(ele[0][0], ele[0][1])
+
+
+def plot_mag_phase(filters, fs):
+    for fi in filters:
+        w, h = signal.freqz_zpk(fi[0][0], fi[0][1], fi[0][2], fs=fs)
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax1.set_title('Digital filter frequency response')
+        ax1.plot(w, 20 * np.log10(abs(h)), 'b')
+        ax1.set_ylabel('Amplitude [dB]', color='b')
+        ax1.set_xlabel('Frequency [Hz]')
+        ax1.grid()
+        ax2 = ax1.twinx()
+        angles = np.unwrap(np.angle(h))
+        ax2.plot(w, angles, 'g')
+        ax2.set_ylabel('Angle [radians]', color='g')
+        plt.axis('tight')
+    plt.show()
+
+
+def plot_impl_unitstep(filters):
+    for filter in filters:
+        impulse = np.repeat(0., 60)
+        impulse[0] = 1.
+        x = np.arange(0, 60)
+        coff = signal.ZerosPolesGain(filter[0][0], filter[0][1], filter[0][2])
+        # Compute the impulse response
+        response = signal.lfilter(coff.to_tf().num, coff.to_tf().den, impulse)
+        # Plot filter impulse and step response:
+        fig = plt.figure(figsize=(10, 6))
+        plt.subplot(211)
+        plt.stem(x, response, 'm', use_line_collection=True)
+        plt.ylabel('Amplitude', fontsize=15)
+        plt.xlabel(r'n (samples)', fontsize=15)
+        plt.title(r'Impulse response', fontsize=15)
+        plt.subplot(212)
+        step = np.cumsum(response)
+        # Compute step response of the system
+        plt.stem(x, step, 'g', use_line_collection=True)
+        plt.ylabel('Amplitude', fontsize=15)
+        plt.xlabel(r'n (samples)', fontsize=15)
+        plt.title(r'Step response', fontsize=15)
+        plt.subplots_adjust(hspace=0.5)
+        fig.tight_layout()
+    plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import soundfile as sf
 import numpy as np
-from helper_method import get_bands, iir_filter
+from helper_method import get_bands, plot_mag_phase
+from helper_method import iir_filter, plot_impl_unitstep
+from helper_method import plot_zeros_poles
 from tkinter import filedialog
 from scipy import signal
 
@@ -16,21 +18,27 @@ print(f"Data dimensions: {np.shape(data)}")
 print(f"Frequency: {fs}")
 
 bands = get_bands()
-gains = []
-for band in bands:
-    gain = int(input(f"Enter the gain (in dB) for band {band}: "))
-    gains.append(gain)
+# gains = []
+# for band in bands:
+#     gain = int(input(f"Enter the gain (in dB) for band {band}: "))
+#     gains.append(gain)
 
 filter_type = input("Enter filter type (iir or fir): ")
 output_fs = int(input("Enter the output sample rate: "))
 
-order = 100
+order = 2
+filters = None
 if filter_type == 'iir':
-    # Figure out if we want to use fs from user input here..
-    filters = iir_filter(order, fs)
+    filters = iir_filter(order, output_fs)
 elif filter_type == 'fir':
     pass
-
+plot_zeros_poles(filters)
+plot_mag_phase(filters, output_fs)
+plot_impl_unitstep(filters)
 output = np.zeros_like(data)
 for filter in filters:
-    output = output + signal.lfilter(filter[0][0], filter[0][1], data)
+    x = signal.ZerosPolesGain(filter[0][0], filter[0][1], filter[0][2])
+    output = output + signal.lfilter(x.to_tf().num, x.to_tf().den, data)
+
+output_file_name = filedialog.asksaveasfilename()
+sf.write(output_file_name, output, output_fs)
